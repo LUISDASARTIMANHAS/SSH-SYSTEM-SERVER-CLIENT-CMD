8de3bae58e2c551432f8411560f539ab2d12192f
LUIS_DAS_ARTIMANHAS <75493473+LUISDASARTIMANHAS@users.noreply.github.com>
2024-02-16 14:01:34 -0300

    Add files via upload

--- SSH SYSTEM/main.cmd
+++ SSH SYSTEM/main.cmd
@@ -1,4 +1,5 @@
 @echo off
+color 0A
 setlocal EnableDelayedExpansion
 set count=0
 
@@ -8,19 +9,18 @@ for /f "tokens=2 delims=:" %%A in ('ipconfig ^| find "IPv4"') do (
 )
 
 :menu
+color 0A
 cls
-echo Criado e Desenvolvido Por LUIS DAS ARTIMANHAS e PINGOBRAS S.A
+echo =================================================================
+echo  Criado e Desenvolvido Por LUIS DAS ARTIMANHAS e PINGOBRAS S.A
+echo   [1] Iniciar Server SSH        [2] Parar Server SSH
+echo   [3] Conectar SSH Modo Client  [4] Auto Conectar SSH Modo Client
+echo                          [5] Status do Servidor
+echo                          [0] Sair
+echo =================================================================
 echo.
 echo IP DEFINIDO DO SERVIDOR: %IP%
 echo CLIENTE LOCAL DEFINIDO: %user%
-echo.
-echo Digite o numero da opcao:
-echo 1 - Iniciar Server SSH
-echo 2 - Parar Server SSH
-echo 3 - Conectar SSH Modo Client
-echo 4 - Auto Conectar SSH Modo Client
-echo 5 - Status do Servidor
-echo 0 - Sair
 echo.
 
 set /p opcao=Insira a opcao: 
@@ -29,12 +29,14 @@ if "%opcao%"=="1" (
 	net start sshd
 	sc config sshd start=auto
 	sc query sshd
+
 	pause
     	goto menu
 
 ) else if "%opcao%"=="2" (
 	net stop sshd
 	sc query sshd
+
     	pause
     	goto menu
 
@@ -52,23 +54,29 @@ if "%opcao%"=="1" (
 		echo Conectando se ao IP[%%i]: !AUTOIP[%%i]:~1!
 		ssh -X %user%@!AUTOIP[%%i]:~1!
 	)
+
     	pause
 	goto menu
 
 ) else if "%opcao%"=="5" (
 	sc query sshd
+
     	pause
     	goto menu
 
 ) else if "%opcao%"=="0" (
-    	echo Criado e Desenvolvido Por LUIS DAS ARTIMANHAS e PINGOBRAS S.A
+	echo =================================================================
+    	echo   Criado e Desenvolvido Por LUIS DAS ARTIMANHAS e PINGOBRAS S.A
+	echo =================================================================
     	echo Saindo...
+
     	pause
 	net stop sshd
     	exit
 
 ) else (
     	echo Opcao invalida. Tente novamente.
+
 	pause
     	goto menu
 )
